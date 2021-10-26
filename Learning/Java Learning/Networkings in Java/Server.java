4dd72f4048c8540467d2730daa468788db5099d1
sthiyanesh <sthiyanesh1@gmail.com>
2021-10-26 20:09:39 +0530

    Java learning

---
+++ Learning/Java Learning/Networkings in Java/Server.java
@@ -0,0 +1,24 @@
+import java.lang.*;
+import java.util.*;
+import java.io.*;  
+import java.net.*;  
+public class Server {  
+	public static void main(String[] args){  
+		try{  
+			ServerSocket ss=new ServerSocket(6666);
+			System.out.println("Waiting for Client");
+			Socket s=ss.accept();
+			System.out.println("Connected to Client");
+			DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Enter Message for Client:");
+			String s1 = sc.nextLine();
+			dos.writeUTF(s1);  
+			dos.flush();  
+			dos.close();			
+			ss.close();  
+		}catch(Exception e){
+			System.out.println(e);
+		}			
+	}  
+}  
