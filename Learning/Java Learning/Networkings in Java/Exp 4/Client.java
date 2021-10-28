37f18b7d8fa654cf38c8dc97e21285426c75c992
sthiyanesh <sthiyanesh1@gmail.com>
2021-10-28 21:38:10 +0530

    Learning Networking using Java

---
+++ Learning/Java Learning/Networkings in Java/Exp 4/Client.java
@@ -0,0 +1,36 @@
+import java.lang.*;
+import java.util.*;
+import java.io.*;  
+import java.net.*;  
+public class Client {  
+	public static void main(String[] args) {  
+		try{      
+			Socket s=new Socket("localhost",6666);
+			System.out.println("Connected to Server");
+			DataInputStream din=new DataInputStream(s.getInputStream());
+			DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+			Scanner sc = new Scanner(System.in);
+			String s1;
+			while(true){
+				System.out.println("Waiting for Message from Server...");
+				s1 =(String)din.readUTF();  
+				System.out.println("message= "+ s1);
+				if(s1.equals("End")){
+					System.out.println("Server Reset the Connecetion");
+					break;
+				}
+				System.out.print("Enter Message for Server:");
+				s1 = sc.nextLine();
+				dout.writeUTF(s1);  
+				dout.flush();
+				if(s1.equals("End")){
+					System.out.println("You Reset the Connection");
+					break;
+				}
+			}
+			s.close();  
+		}catch(Exception ex){
+			System.out.println(ex);
+		}  
+	}  
+}  
