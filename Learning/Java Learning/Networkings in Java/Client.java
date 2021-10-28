84cffe1ba34ec1d319303398e11ee07133b80ac3
sthiyanesh <sthiyanesh1@gmail.com>
2021-10-28 21:35:31 +0530

    Learning Networking using Java

---
+++ Learning/Java Learning/Networkings in Java/Client.java
@@ -0,0 +1,17 @@
+import java.io.*;  
+import java.net.*;  
+public class Client {  
+	public static void main(String[] args) {  
+		try{      
+			Socket s=new Socket("localhost",6666);
+			System.out.println("Connected to Server");
+			DataInputStream din=new DataInputStream(s.getInputStream());  
+			System.out.println("Waiting for Message from Server...");
+			String s1 =(String)din.readUTF();  
+			System.out.println("message= "+ s1);  
+			s.close();  
+		}catch(Exception ex){
+			System.out.println(ex);
+		}  
+	}  
+}  
