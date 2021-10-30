aae8b6727c3017190d8203cc102d6bd340f09f3f
sthiyanesh <sthiyanesh1@gmail.com>
2021-10-30 20:15:40 +0530

    Learning Networking in Java

---
+++ Learning/Java Learning/Networkings in Java/ChatAppClient.java
@@ -0,0 +1,99 @@
+import java.net.*;
+import java.io.*;
+public class ChatAppClient {
+    private String hostname;
+    private int port;
+    private String userName; 
+    public ChatAppClient(String hostname, int port) {
+        this.hostname = hostname;
+        this.port = port;
+    }
+    public void execute() {
+        try {
+            Socket socket = new Socket(hostname, port);
+            System.out.println("Connected to the chat server");
+            new ReadThread(socket, this).start();
+            new WriteThread(socket, this).start();
+        } catch (UnknownHostException ex) {
+            System.out.println("Server not found: " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("I/O Error: " + ex.getMessage());
+        }
+    }
+    void setUserName(String userName) {
+        this.userName = userName;
+    }
+    String getUserName() {
+        return this.userName;
+    }
+    public static void main(String[] args) {
+        if (args.length < 2) return;
+        String hostname = args[0];
+        int port = Integer.parseInt(args[1]);
+        ChatAppClient client = new ChatAppClient(hostname, port);
+        client.execute();
+    }
+}
+class ReadThread extends Thread {
+    private BufferedReader reader;
+    private Socket socket;
+    private ChatAppClient client;
+    public ReadThread(Socket socket, ChatAppClient client) {
+        this.socket = socket;
+        this.client = client;
+        try {
+            InputStream input = socket.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(input));
+        } catch (IOException ex) {
+            System.out.println("Error getting input stream: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+    public void run() {
+        while (true) {
+            try {
+                String response = reader.readLine();
+                System.out.println("\n" + response);
+                if (client.getUserName() != null) {
+                    System.out.print("[" + client.getUserName() + "]: ");
+                }
+            } catch (IOException ex) {
+                System.out.println("Error reading from server: " + ex.getMessage());
+                ex.printStackTrace();
+                break;
+            }
+        }
+    }
+}
+class WriteThread extends Thread {
+    private PrintWriter writer;
+    private Socket socket;
+    private ChatAppClient client;
+    public WriteThread(Socket socket, ChatAppClient client) {
+        this.socket = socket;
+        this.client = client;
+        try {
+            OutputStream output = socket.getOutputStream();
+            writer = new PrintWriter(output, true);
+        } catch (IOException ex) {
+            System.out.println("Error getting output stream: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+    public void run() {
+        Console console = System.console();
+        String userName = console.readLine("\nEnter your name: ");
+        client.setUserName(userName);
+        writer.println(userName);
+        String text;
+        do {
+            text = console.readLine("[" + userName + "]: ");
+            writer.println(text);
+        } while (!text.equals("bye"));
+        try {
+            socket.close();
+        } catch (IOException ex) {
+            System.out.println("Error writing to server: " + ex.getMessage());
+        }
+    }
+}
