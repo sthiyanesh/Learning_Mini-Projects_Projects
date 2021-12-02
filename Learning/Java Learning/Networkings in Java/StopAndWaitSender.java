d1d670922d5260052df70c6d5373f2c9af1515e8
sthiyanesh <sthiyanesh1@gmail.com>
2021-12-03 00:04:38 +0530

    Learning Networking using Java

---
+++ Learning/Java Learning/Networkings in Java/StopAndWaitSender.java
@@ -0,0 +1,24 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+public class StopAndWaitSender{
+    public static void main(String Args[]){
+        Socket s = new Socket("localhost",6666);
+        System.out.println("Connected to Receiver...");
+        DataInputStream din = new DataInputStream(s.getInputStream());
+        DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+        System.out.println("Enter the No.of.Frames:");
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        sc.nextLine();
+        String msg;
+        for(int i=0;i<len;i++){
+            System.out.println("Enter Data for "+(i+1)+"th Frame:");
+            msg = sc.nextLine();
+            dout.writeUTF(msg);
+            System.out.println("Frame Sent.");
+            int ack = Integer.parseInt(din.readUTF());
+            System.out.println("The Acknowledgemnt received for "+(ack)+"th frame");
+        }
+    }
+}
