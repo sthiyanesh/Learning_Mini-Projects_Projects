5a65f8fcb3fe85f2bf111f167e9c5036a333c031
sthiyanesh <sthiyanesh1@gmail.com>
2021-12-05 22:14:47 +0530

    Learning networking using Java

---
+++ MiniProjects/Messenger App/pros.java
@@ -0,0 +1,89 @@
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+class pros{
+public static ServerSocket ss;
+public static Socket s;
+public static Socket s1;
+public static DataOutputStream dos;
+public static DataInputStream din;
+public static String  str = "", str1 = "" , str2 = "" , str3 = "";
+public static int z = 0;
+public static void main(String[] args){
+JFrame ms = new JFrame("MESSENGER server");
+JPanel p2 = new JPanel();
+JTextField tf = new JTextField("");
+JButton b1 = new JButton("SEND");
+JTextArea ta = new JTextArea();
+JScrollPane sp = new JScrollPane(ta,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+            JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+sp.setBounds(5,25,621,550);
+ta.setEditable(false);
+b1.setBounds(521,575,100,25);
+tf.setEditable(true);
+tf.setBounds(5,575,521,25);
+p2.setBounds(5,0,621,25);
+p2.setBackground(Color.red);
+ms.setSize(650,650);
+ms.setVisible(true);
+ms.setLayout(null);
+ms.add(p2);
+ms.add(tf);
+ms.add(b1);
+ms.add(sp);
+b1.addActionListener(new ActionListener() {
+public void actionPerformed(ActionEvent e){
+try{
+str1 = tf.getText();
+dos.writeUTF(str1);
+dos.flush();
+str3 = ta.getText();
+str3 += "\nYou = "+str1;
+ta.setText(str3);
+tf.setText("");
+}
+catch(Exception ex){System.out.print(ex);}  
+}
+} );
+try{
+s1 =new Socket("localhost",8888);
+}
+catch(Exception ex){
+try{
+ss = new ServerSocket(6666);
+s = ss.accept();
+z = 1;
+}
+catch(Exception E){
+System.out.println(E);
+}
+}
+try{
+if (z == 1){
+din=new DataInputStream(s.getInputStream());  
+dos=new DataOutputStream(s.getOutputStream());
+}
+else {
+din=new DataInputStream(s1.getInputStream());  
+dos=new DataOutputStream(s1.getOutputStream());
+}
+while (!str1.equals("stop")){
+str = (String)din.readUTF();
+str3 = ta.getText();
+str3 += "\nClient = "+str;
+ta.setText(str3);
+           }
+  din.close();
+  dos.close();
+  s.close();
+  ss.close();
+}
+catch(Exception ex){
+System.out.println(ex);
+}  
+}
+}
